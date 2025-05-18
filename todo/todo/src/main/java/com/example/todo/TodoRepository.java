051dd7e02e7a79d8f2873ef813f42f7c1123c2b9
DevineC45 <conordevine11@icloud.com>
2025-05-18 12:13:08 +0100

    Cleaned up backend code

--- todo/todo/src/main/java/com/example/todo/TodoRepository.java
+++ todo/todo/src/main/java/com/example/todo/TodoRepository.java
@@ -1,8 +1,6 @@
 package com.example.todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-//"Spring, generate all the standard database operations (CRUD) for the TodoItem entity"
-
 public interface TodoRepository extends JpaRepository<TodoItem, Long> {
-    // No need to write any methods — JpaRepository gives you all the basics
+    // No need to write any methods — JpaRepository gives me all the basics
 }
